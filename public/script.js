75ba7869924e53ba3aa695099cb7b06cced386c5
Yankı Kırlıkova <yanki.kirlikova@gmail.com>
2021-08-02 12:05:44 +0300

    first

---
+++ public/script.js
@@ -0,0 +1,28 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA4hIqLYzmOJE4ZcZ7N3I1LO9CkozJ2kr0",
+  authDomain: "arda-twitter.firebaseapp.com",
+  databaseURL: "https://arda-twitter.firebaseio.com", //https://arda-twitter-default-rtdb.europe-west1.firebasedatabase.app
+  projectId: "arda-twitter",
+  storageBucket: "arda-twitter.appspot.com",
+  messagingSenderId: "6702542527",
+  appId: "1:6702542527:web:b0334bd11dfb96a358600a",
+  measurementId: "G-31LD0WNXZC",
+};
+
+
+
+var username = document.getElementById("uname").value;
+var password = document.getElementById("password").value;
+var database = firebase.database();
+
+function writeUserData(username,password) {
+  firebase.database().ref('users/' + userId).set({
+    username: username,
+    password: password,
+  });
+}
+
+
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+writeUserData(username,password);
---
+++ script.js
@@ -0,0 +1,25 @@
+/*const firebaseConfig = {
+    apiKey: "AIzaSyCEKpeROzNe7W2YffsO4-PWSmmIb78-fj0",
+    authDomain: "sharcscout3.firebaseapp.com",
+    databaseURL: "https://sharcscout3.firebaseio.com",
+    projectId: "sharcscout3",
+    storageBucket: "sharcscout3.appspot.com",
+    messagingSenderId: "839512931875",
+    appId: "1:839512931875:web:002133feb48210323e3b96",
+   };*/
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA4hIqLYzmOJE4ZcZ7N3I1LO9CkozJ2kr0",
+  authDomain: "arda-twitter.firebaseapp.com",
+  databaseURL: "https://arda-twitter.firebaseio.com", //https://arda-twitter-default-rtdb.europe-west1.firebasedatabase.app
+  projectId: "arda-twitter",
+  storageBucket: "arda-twitter.appspot.com",
+  messagingSenderId: "6702542527",
+  appId: "1:6702542527:web:b0334bd11dfb96a358600a",
+  measurementId: "G-31LD0WNXZC",
+};
+
+
+  
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
